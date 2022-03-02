9d0693739309e9578986ebf16ee33db8cbadc02b
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-03-02 16:25:27 +0800

    ✨ feat: migrate to ERC721 nft project demo

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var StarDuck = artifacts.require("./StarDuck.sol");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(StarDuck, 1000000000);
+  await deployer.deploy(
+    StarDuck,
+    "Star Duck",
+    "SRD",
+    "https://gateway.pinata.cloud/ipfs/QmYaeMjyhjEeRn2uecZf5CRLAQov6yDa2X5Rq7Jm8zNnC7"
+  );
 };
